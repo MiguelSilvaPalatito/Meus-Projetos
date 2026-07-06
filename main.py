52d3af93917269f401754bf4d61d4f8902689d78
Miguel <miguelhenriquebu0709@gmail.com>
2026-07-06 20:14:14 -0300

    Primeiro save do projeto

---
+++ main.py
@@ -0,0 +1,17 @@
+while True:
+    n1 = int(input("selecione umas das opções: \n1 - aluno \n2 - professor \n3 - planos \n:"))
+
+    if n1 == 1:
+        print("opção 1 em desenvolvimento")
+        break
+
+    elif n1 == 2:
+        print("opção 2 em desenvolvimento")
+        break
+    
+    elif n1 == 3:
+        print("opção 3 em desenvolvimento")
+        break
+
+    else:
+        print("selecione um numero valido!")
